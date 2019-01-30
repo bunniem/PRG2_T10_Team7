be31570aab67e2357b1d96b1f311267dedaa9ff6
Tan Yu Quan <s10186868@connect.np.edu.sg>
2019-01-30 15:11:11 +0800

    implement fix for NoOfOccupants not working in backBtn2

--- PRG2_ASSIGNMENT/MainPage.xaml.cs
+++ PRG2_ASSIGNMENT/MainPage.xaml.cs
@@ -931,7 +931,11 @@ namespace PRG2_ASSIGNMENT
                 /* Set addon booleans for rooms to false */
                 if (r is DeluxeRoom dr)
                 {
+                    if (dr.AdditionalBed == true)
+                    {
                     dr.AdditionalBed = false;
+                    dr.NoOfOccupants -= 1;
+                    }
                 }
                 else if (r is StandardRoom sr)
                 {
